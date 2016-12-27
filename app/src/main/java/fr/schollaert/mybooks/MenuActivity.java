88f3931b7cb09981b226c1ce8c55148ff66dd37a
Toto <schollaert2@gmail.com>
2016-12-27 18:58:47 +0100

    check connection before launch menu or login page

--- app/src/main/java/fr/schollaert/mybooks/MenuActivity.java
+++ app/src/main/java/fr/schollaert/mybooks/MenuActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MenuActivity extends AppCompatActivity implements  View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -15,6 +19,9 @@ public class MenuActivity extends AppCompatActivity implements  View.OnClickList
         findViewById(R.id.myBooksButton).setOnClickListener(this);
         findViewById(R.id.findBookButton).setOnClickListener(this);
         findViewById(R.id.myCommentButton).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -29,6 +36,9 @@ public class MenuActivity extends AppCompatActivity implements  View.OnClickList
             startActivity(new Intent(this, FindBooksActivity.class));
         }else if (i == R.id.myCommentButton) {
             startActivity(new Intent(this, MyCommentsActivity.class));
+        } else if(i == R.id.sign_out_button){
+            mAuth.signOut();
+            startActivity(new Intent(this, EmailPasswordActivity.class));
         }
     }
 }
---
+++ app/src/main/java/fr/schollaert/mybooks/checkConnectionActivity.java
@@ -0,0 +1,25 @@
+package fr.schollaert.mybooks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CheckConnectionActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_connection);
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(this, MenuActivity.class));
+        }
+        else{
+            startActivity(new Intent(this, EmailPasswordActivity.class));
+        }
+    }
+}
